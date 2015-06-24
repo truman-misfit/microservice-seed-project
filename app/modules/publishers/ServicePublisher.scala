b602a6768276f63abd160ec3ead48b039e4d1f89
trumanw <wuchunan@wuchunandeMacBook-Pro.local>
2015-06-24 18:51:20 +0800

    re-factory publisher with multiple async client and lazy loading mechanism.

--- app/modules/publishers/ServicePublisher.scala
+++ app/modules/publishers/ServicePublisher.scala
@@ -2,6 +2,14 @@ package com.misfit.microservices.modules
 
 import java.nio.ByteBuffer
 
+import scala.concurrent.Future
+import scala.util.Try
+
 trait ServicePublisher {
 	def put(data: ByteBuffer)
+}
+
+trait ServicePublisherAbstract {
+	def publish(service: String, content: String): Unit
+	def shutdown(): Unit
 }
